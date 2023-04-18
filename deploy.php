be8524f81d9bfaf35330fa410e1b08c5e489efb1
florianne-gb <florianne.gras@gmail.com>
2023-04-18 11:20:13 +0200

    initialisation du projet

--- deploy.php
+++ deploy.php
@@ -7,17 +7,24 @@ require 'recipe/symfony.php';
 set('repository', 'git@github.com:CelluleDigitale/gamme-prepa.git');
 set('http_user', 'ibepform-cellule');
 
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    set('git_tty', false);
+    set('ssh_multiplexing', false);
+}
+
 set('keep_releases', 3);
 
 add('shared_dirs', [  'var/log',
 'public/uploads',]);
 set('allow_anonymous_stats', false);
 
+
+
 // Hosts
 host('preprod')
     ->setHostname('ftp.cluster003.hosting.ovh.net')
-    ->set('remote_user','ibepform-cellule')
-    ->set('deploy_path', '~/landingpage/preprod-prepa-projet')
+    ->setRemoteUser('ibepform-cellule')
+    ->set('deploy_path', '~/landingpage/preprod-prepaprojet')
     ->set('branch', 'develop')
 ;
 
