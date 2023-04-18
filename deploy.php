f6a2aaef1497a4589e29966a9614954f6f7a8855
florianne-gb <florianne.gras@gmail.com>
2023-04-18 14:44:40 +0200

    initialisation du projet

--- deploy.php
+++ deploy.php
@@ -10,19 +10,42 @@ set('application', 'gamme-prepa');
 
 set('repository', 'git@github.com:CelluleDigitale/gamme-prepa.git');
 set('http_user', 'ibepform-cellule');
-set('git_ssh_command', 'ssh');
+// Windows Compatibility
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
     set('git_tty', false);
     set('ssh_multiplexing', false);
-    set('use_ssh_agent', true);
-    set('git_ssh_command', 'ssh');
 }
 
 set('keep_releases', 3);
 
-add('shared_dirs', [  'var/log',
-'public/uploads',]);
+set('shared_dirs', [
+    'var/log',
+    'public/uploads',
+]);
+
 set('allow_anonymous_stats', false);
+
+set('bin/php', function() {
+    return '/opt/php8.0/bin/php';
+});
+
+set('bin/composer', function () {
+    return '/opt/php8.0/bin/php /opt/php8.0/bin/composer.phar';
+});
+
+
+// Tâches customs
+desc('View migrations\'s status');
+task('database:status', function () {
+    run("cd {{release_or_current_path}} && {{bin/console}} doctrine:migrations:status {{console_options}}");
+});
+
+desc('Compile .env files to improve performance');
+task('deploy:dump-env', function () {
+    run('cd {{release_path}} && {{bin/composer}} dump-env prod');
+});
+
+
 
 
 
@@ -33,6 +56,7 @@ host('preprod')
     ->set('deploy_path', '~/landingpage/preprod-prepaprojet')
     ->set('branch', 'develop')
 ;
+before('deploy:symlink', 'deploy:dump-env');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
