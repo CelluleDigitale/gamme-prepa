2730ef5478ad714a08a4ea41812650ea596700e8
florianne-gb <florianne.gras@gmail.com>
2023-04-18 16:03:35 +0200

    initialisation du projet

--- deploy.php
+++ deploy.php
@@ -21,6 +21,15 @@ set('shared_dirs', [
 ]);
 set('allow_anonymous_stats', false);
 
+set('bin/php', function() {
+    return '/opt/php8.0/bin/php';
+});
+
+set('bin/composer', function () {
+    return '/opt/php8.0/bin/php /opt/php8.0/bin/composer.phar';
+});
+
+
 // Hosts
 host('preprod')
     ->setHostname('ftp.cluster003.hosting.ovh.net')
