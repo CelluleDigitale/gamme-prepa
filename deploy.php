fb31eb0b76a62290fed0aa589ba1dcab4da26c7c
florianne-gb <florianne.gras@gmail.com>
2023-04-18 12:20:07 +0200

    initialisation du projet

--- deploy.php
+++ deploy.php
@@ -9,13 +9,12 @@ set('application', 'gamme-prepa');
 // Project repository
 set('repository', 'git@github.com:CelluleDigitale/gamme-prepa.git');
 set('http_user', 'ibepform-cellule');
-set('ssh_multiplexing', false);
-set('use_ssh_agent', true);
-set('git_ssh_command', 'ssh');
+
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
     set('git_tty', false);
     set('ssh_multiplexing', false);
     set('use_ssh_agent', true);
+    set('git_ssh_command', 'ssh');
 }
 
 set('keep_releases', 3);
